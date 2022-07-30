b78e7eedc8edf00342a2b5bf2b1414c925c7a7a6
Spencer Lepine <spencerlepine26@gmail.com>
2022-07-29 23:59:16 -0700

    use Cobertura XML test coverage output

--- gateway/jest.config.js
+++ gateway/jest.config.js
@@ -1,5 +1,4 @@
 module.exports = {
-  setupFilesAfterEnv: ['./tests/suiteSetup.js'],
   collectCoverageFrom: ['src/**/*.js', '!**/node_modules/**'],
   coverageReporters: ['html', 'text', 'text-summary', 'cobertura'],
   testMatch: ['**/*.test.js'],
--- users/jest.config.js
+++ users/jest.config.js
@@ -1,3 +1,6 @@
 module.exports = {
   setupFilesAfterEnv: ['./tests/suiteSetup.js'],
+  collectCoverageFrom: ['src/**/*.js', '!**/node_modules/**'],
+  coverageReporters: ['html', 'text', 'text-summary', 'cobertura'],
+  testMatch: ['**/*.test.js'],
 };
