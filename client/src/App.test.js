bdfd6a9f85b2c44661d72f80ecfa27edffe7ee81
Spencer Lepine <spencerlepine26@gmail.com>
2022-07-31 21:11:38 -0700

    mock react router when testing

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders app name', () => {
+  render(<App />, { wrapper: MemoryRouter });
+  const titleElement = screen.getByText(/SquidShop/i);
+  expect(titleElement).toBeInTheDocument();
 });
