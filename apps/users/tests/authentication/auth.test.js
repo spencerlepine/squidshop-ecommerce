16af9b79208b078aa3d6b83f4535bcd01d473f81
Spencer Lepine <spencerlepine26@gmail.com>
2022-08-05 15:49:00 -0700

    PASS authentication integration test

--- apps/users/tests/authentication/auth.test.js
+++ apps/users/tests/authentication/auth.test.js
@@ -1,5 +1,5 @@
 const request = require('supertest');
-const app = require('../index');
+const app = require('../../index');
 
 jest.setTimeout(10000);
 
--- apps/users/tests/authentication/register.test.js
+++ apps/users/tests/authentication/register.test.js
@@ -1,5 +1,5 @@
 const request = require('supertest');
-const app = require('../index');
+const app = require('../../index');
 
 describe('/register endpoint', () => {
   test('should respond', (done) => {
