9281b15a4aac38cd42c336fa871828ba63a7ba84
Spencer Lepine <spencerlepine26@gmail.com>
2022-08-09 10:04:53 -0700

    test for no duplicate email registration

--- apps/users/src/controllers/register/index.js
+++ apps/users/src/controllers/register/index.js
@@ -7,27 +7,39 @@ const db = require('../../database');
 const { User } = db;
 
 // eslint-disable-next-line consistent-return
-router.post('/', async (req, res) => {
-  try {
-    if (!req.body.password || !req.body.email) {
-      return res.sendStatus(401); // redirect('/register');
-    }
-
-    const hashPassword = await bcrypt.hash(req.body.password, 10);
-    const userDetails = {
-      email: req.body.email,
-      password: hashPassword,
-    };
-
-    // To get the Id from the newly created user
-    // .then((data) => data.dataValues.id)
-    // const newUserId = data.dataValues.id;
-    return User.create(userDetails)
-      .then(() => res.sendStatus(201)) // res.redirect('/login'))
-      .catch(() => res.sendStatus(401)); // res.redirect('/register'));
-  } catch (e) {
-    return res.sendStatus(401);// res.redirect('/register');
+router.post('/', (req, res) => {
+  if (!req.body.password || !req.body.email) {
+    return res.sendStatus(401);
   }
+
+  return User.findAll({
+    where: {
+      email: req.body.email,
+    },
+  })
+    .then(async (data) => {
+      const existingUser = data && data.length === 1 && data[0].dataValues;
+      if (!existingUser) {
+        const hashPassword = await bcrypt.hash(req.body.password, 10);
+        const userDetails = {
+          email: req.body.email,
+          password: hashPassword,
+        };
+
+        // To get the Id from the newly created user
+        // .then((data) => data.dataValues.id)
+        // const newUserId = data.dataValues.id;
+        return User.create(userDetails)
+          .then(() => res.sendStatus(201))
+          .catch((err) => res.status(500).json({ error: err }));
+      }
+
+      return res.sendStatus(401);
+    })
+    .catch((err) => {
+      console.error(err);
+      return res.sendStatus(401);
+    });
 });
 
 module.exports = router;
--- apps/users/tests/authentication/register.test.js
+++ apps/users/tests/authentication/register.test.js
@@ -13,8 +13,24 @@ describe('/register endpoint', () => {
   test('should revoke invalid data', (done) => {
     request(app)
       .post('/register')
-      .send({ email: 'testUser', password: '' })
+      .send({ email: 'testemail@gmail.com', password: '' })
       .expect(401)
+      .then(() => done());
+  });
+
+  test('should not sign up duplicate email', (done) => {
+    const duplicateEmail = 'duplicateemail@gmail.com';
+
+    request(app)
+      .post('/register')
+      .send({ email: duplicateEmail, password: 'password$948' })
+      .expect(201)
+      .then(() => (
+        request(app)
+          .post('/register')
+          .send({ email: duplicateEmail, password: 'password$948' })
+          .expect(401)
+      ))
       .then(() => done());
   });
 });
