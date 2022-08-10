e1896ecad04a26b0c07353344bd3e5d08bff63bf
Spencer Lepine <spencerlepine26@gmail.com>
2022-08-10 14:47:56 -0700

    add error handlers to Gateway API

--- apps/gateway/index.js
+++ apps/gateway/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
@@ -45,6 +46,26 @@ app.get('/status', (req, res) => {
     config,
   });
 });
+
+// Error Handlers
+const errorLogger = (error, req, res, next) => { // for logging errors
+  console.error(error); // or using any fancy logging library HERE TODO
+  next(error); // forward to next middleware
+};
+
+const errorResponder = (error, req, res, next) => { // responding to client
+  if (error.type === 'time-out') { // arbitrary condition check
+    res.status(408).send(error);
+  } else { next(error); } // forwarding exceptional case to fail-safe middleware
+};
+
+const failSafeHandler = (error, req, res, next) => { // generic handler
+  res.status(500).send(error);
+};
+
+app.use(errorLogger);
+app.use(errorResponder);
+app.use(failSafeHandler);
 
 // Start the Server
 const { PORT } = config;
