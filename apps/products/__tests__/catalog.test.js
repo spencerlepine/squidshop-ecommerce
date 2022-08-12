a6c7454a94c6a6289b8a6431a6879041dd7633f2
Spencer Lepine <spencerlepine26@gmail.com>
2022-08-12 13:29:09 -0700

    write catalog test

--- apps/products/__tests__/catalog.test.js
+++ apps/products/__tests__/catalog.test.js
@@ -3,30 +3,30 @@ const request = require('supertest');
 const app = require('../index');
 const mockProduct = require('./mockProduct.json');
 
+const uploadProduct = () => new Promise((resolve) => (
+  request(app)
+    .post('/product/upload')
+    .send(mockProduct)
+    .then((res) => resolve(res.body.productId))
+));
+
 describe('/catalog endpoint', () => {
   test('should respond with list of products', (done) => {
-    const uploadProduct = () => new Promise((resolve) => (
-      request(app)
-        .post('/product/upload')
-        .send(mockProduct)
-        .then((res) => resolve(res.body.productId))
-    ));
-
-    const fetchCatalog = () => new Promise((resolve) => (
-      request(app)
-        .get('/catalog')
-        .expect(200)
-        .then((response) => {
-          expect(typeof response.body).toBe('object');
-          resolve();
-        })
-    ));
-
     uploadProduct()
       .then(uploadProduct)
       .then(uploadProduct)
       .then(uploadProduct)
-      .then(fetchCatalog)
+      .then(() => (
+        request(app)
+          .get('/catalog')
+          .expect(200)
+          .then((response) => {
+            expect(typeof response.body).toBe('object');
+            expect(response.body).toHaveProperty('products');
+            expect(response.body.length > 1).toBeTruthy();
+            resolve();
+          })
+      ))
       .then(() => done())
       .catch((err) => done(err));
   });
