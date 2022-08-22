e2c2e2716c72e69a115d6421554cdba03c62bc78
Spencer Lepine <spencerlepine26@gmail.com>
2022-08-22 13:25:18 -0700

    remove broken react env for now

--- apps/client/src/App.js
+++ apps/client/src/App.js
@@ -4,7 +4,6 @@ import Footer from './components/Footer';
 import CssBaseline from '@mui/material/CssBaseline';
 import Box from '@mui/material/Box';
 import './App.css';
-import config from './config'; // TODO delete this
 
 const containerStyling = {
   display: 'flex',
@@ -20,7 +19,7 @@ const App = () => {
 
       <Box sx={containerStyling}>
         <CssBaseline />
-        <p>TODO ${config.REACT_APP_GATEWAY_API_URL}, {process.env.NODE_ENV}</p>
+
         <PageRoutes />
 
         <Footer />
--- apps/client/src/config.js
+++ apps/client/src/config.js
@@ -1,6 +1,5 @@
 let REACT_APP_GATEWAY_API_URL = 'http://localhost:5000'
 
-
 if (process.env.REACT_APP_GATEWAY_API_URL) {
   REACT_APP_GATEWAY_API_URL = process.env.REACT_APP_GATEWAY_API_URL;
 }
