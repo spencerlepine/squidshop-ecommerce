49de46be88153509f635ba328344628a4cf504b9
Spencer Lepine <spencerlepine26@gmail.com>
2022-08-23 11:13:02 -0700

    unit testing the registration input data

--- apps/users/src/controllers/register/UserData/UserData.test.js
+++ apps/users/src/controllers/register/UserData/UserData.test.js
@@ -1,1 +1,77 @@
 const UserData = require('./index');
+
+const validMockUser = {
+  firstName: 'Danny',
+  lastName: 'Devito',
+  email: 'devito@urmom.com',
+  password: 'T0ttallY#ArdPa55$'
+}
+
+describe('UserData Class', () => {
+  it('should store user registration data', () => {
+    const userData = new UserData(validMockUser)
+    expect(userData.getData()).toMatchObject(validMockUser)
+  });
+
+  it('should only accept valid name values', () => {
+    const validNames = ['Danny', 'Duncan', 'Ross', 'Creations'];
+    const invalidNames = ['', '99209837NOTMY REALNAME', ') DROP TABLE', '#nope', 'Ihaveaverylongnamebutitisokay'];
+    validateFormTest('firstName', validNames, invalidNames);
+  });
+
+  it('should only accept valid email values', () => {
+    const validEmails = [
+      // TODO
+    ];
+    const invalidEmails = [
+      '',
+      'this is def not an email',
+      'mysite.ourearth.com',
+      'mysite@.com.my',
+      '@you.me.net',
+      'mysite123@gmail.b',
+      'mysite@.org.org',
+      '.mysite@mysite.org',
+      'mysite()*@gmail.com',
+      'mysite..1234@yahoo.com'
+    ];
+
+    validateFormTest('email', validEmails, invalidEmails);
+  });
+
+  it('should only accept valid password values', () => {
+    //  Minimum eight characters, at least one letter, one number and one special character:
+    const validPasses = [
+      '$nA$98488',
+      '$Dd00pqRS1',
+      'T0ttallY#ArdPa55$'
+    ];
+    const invalidPasses = [
+      '',
+      'this is def not a password',
+      '$nN0PP', // below min 8 characters
+      '$98012399', // no letter
+      '$nNiud$$$', // no number
+      '94747nfgjfh4', // no special
+    ];
+    validateFormTest('password', validPasses, invalidPasses);
+  });
+
+  it('should be hashing passwords after validating', () => {
+    const validPass = new UserData(validMockUser)
+    expect(validPass.validateUserData()).toBeTruthy()
+    expect(validPass.getData()).not.toBe(validMockUser.password) // un-hashed
+  });
+})
+
+function validateFormTest(formDataName, validInputs, invalidInputs) {
+  const testList = (expectedResult, list) => (
+    list.forEach((input) => {
+      const data = new UserData({ ...validMockUser, [formDataName]: input })
+      expect(data.validateUserData()).toBe(expectedResult);
+    })
+  );
+
+  testList(true, validInputs);
+  testList(false, invalidInputs);
+}
--- apps/users/src/controllers/register/UserData/index.js
+++ apps/users/src/controllers/register/UserData/index.js
@@ -1,10 +1,10 @@
 const bcrypt = require('bcrypt');
 
 // Minimum eight characters, at least one letter, one number and one special character:
-const passwordRegex = new RegExp(/"^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$"/)
+const passwordRegex = new RegExp(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/)
 // Support international names
 const nameRegex = new RegExp(/^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]+$/u)
-const emailRegex = new RexExp(/(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/)
+const emailRegex = new RegExp(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)
 
 class User {
   constructor({ firstName, lastName, email, password }) {
@@ -23,7 +23,7 @@ class User {
   }
 
   validateName(nameStr) {
-    return nameRegex.test(nameStr)
+    return nameRegex.test(nameStr) && nameStr.length <= 25 && nameStr.length > 0
   }
 
   validateEmail(emailStr) {
@@ -33,12 +33,14 @@ class User {
   validateUserData() {
     if (this.validPassword(this.password)) {
       this.generateHashedPassword()
+    } else {
+      return false
     }
 
-    return this.validName(this.firstName) && this.validName(this.lastName) && this.validEmail(this.email)
+    return this.validateName(this.firstName) && this.validateName(this.lastName) && this.validateEmail(this.email)
   }
 
-  generateHashedPassword() {
+  async generateHashedPassword() {
     if (this.passwordValidated && this.passwordNotHashed) {
       const hashPassword = await bcrypt.hash(this.password, 10);
       this.passwordNotHashed = false
