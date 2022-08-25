f6fa23d3c42d9b62fb3046ed8315b8d7428da162
Spencer Lepine <spencerlepine26@gmail.com>
2022-08-25 14:49:50 -0700

    pass all client tests

--- apps/client/src/components/Account/AccountDetails.test.js
+++ apps/client/src/components/Account/AccountDetails.test.js
@@ -1,5 +1,5 @@
-import { render, screen } from '@testing-library/react';
-import AccountDetails from './index';
+import { render } from '@testing-library/react';
+import AccountDetails from './AccountDetails';
 
 test('should render without without throwing an error', () => {
   render(<AccountDetails />);
